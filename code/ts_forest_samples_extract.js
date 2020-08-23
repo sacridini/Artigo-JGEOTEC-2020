077a07a242860c61b423f332e6ec9c75a744bab4
sacridini <eduardolacerdageo@gmail.com>
2020-08-23 02:56:06 -0300

    minor update on ts_forest_samples_extract.js

--- code/ts_forest_samples_extract.js
+++ code/ts_forest_samples_extract.js
@@ -1,8 +1,8 @@
 var geet = require('users/elacerda/geet:geet'); 
-var ts_collection = geet.build_annual_landsat_timeseries(roi);
+var ts_collection = geet.build_annual_landsat_timeseries(bhrsj);
 
 var clip_image = function(image) {
-  return image.clip(roi);
+  return image.clip(bhrsj);
 };
 var ts_clipped = ts_collection.map(clip_image);
 
@@ -13,15 +13,16 @@ var ts_stack = ts_clipped.iterate(merge_bands, ee.Image([]));
 
 var ts_final = ee.Image(ts_stack)
 
-print(ts_final)
 
 var input_features = ts_final.sampleRegions({
-  collection: af_samples,
+  collection: other,
   scale: 30
 });
 
+print(input_features)
+
 Export.table.toDrive({
   collection: input_features,
-  description:'forest_samples',
-  fileFormat: 'SHP'
+  description:'other_wetness',
+  fileFormat: 'CSV'
 });
